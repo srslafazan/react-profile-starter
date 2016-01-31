66f6d58314dcb10ee8a840651aac164b0c1144ac
Shain Lafazan <srslafazan@gmail.com>
2016-01-30 20:43:50 -0800

    layout for contact component

--- src/components/ProfilePage/Contact/Contact.js
+++ src/components/ProfilePage/Contact/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Contact.scss';
+import {Button} from 'react-materialize';
+
 
 class Contact extends Component {
 
@@ -8,6 +10,11 @@ class Contact extends Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
+
+        	<h1 className={s.heading}>Want to work together?</h1>
+        	<h2 className={s.tagline}>I build your dreams in code.</h2>
+        	<Button>Get Started</Button>
+
         </div>
       </div>
     );
