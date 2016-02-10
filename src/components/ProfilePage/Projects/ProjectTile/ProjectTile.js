39c95392e2e036763fef6d5222ad6116a273c298
Shain Lafazan <srslafazan@gmail.com>
2016-02-10 14:19:02 -0800

    cleaned up projects section

--- src/components/ProfilePage/Projects/ProjectTile/ProjectTile.js
+++ src/components/ProfilePage/Projects/ProjectTile/ProjectTile.js
@@ -6,23 +6,36 @@ import {Button, Row, Col} from 'react-materialize';
 class ProjectTile extends Component {
 	render() {
 		return (
-    		<Col s={12} m={3} l={3} className="offset-l1">
-    			<img className={s.projectThumb} src="http://bit.ly/1NIoQcL" />
-    			<h2>Project Name</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sollicitudin sagittis augue, ac pharetra enim convallis ac. Donec cursus libero tortor, eget pretium elit aliquet ut.</p>
-    			<Row>
-    				<Col s={3} m={3} l={3}>
-		    			<img className={s.testmonialImg} src="http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg" />
-	    			</Col>
-	    			<Col s={9} m={9} l={9}>
-		    			<p className={s.testimonialText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-	    			</Col>
-    			</Row>
+
+
+    		<Col s={12} m={4} l={4} className={s.container}>
+    			
+						<Row>
+		    			<h3 className={s.projectTitle}>{this.props.project.name}</h3>
+		    			<p className={s.projectDescription}>{this.props.project.description}</p>
+							<img className={s.projectImage} src={this.props.project.imageUrl} />	
+						</Row>
+					
+    			<img className={s.referenceImg} src={this.props.project.reference.imageUrl} />
+    			<p className={s.referenceQuote}>{this.props.project.reference.quote}</p>
+    			<p className={s.referenceName}>-{this.props.project.reference.name}</p>
+
+
     		</Col>
 
 		)
 	}
 }
+    			// <Row s={12} m={12} l={12} className={s.topRow}>
+	    		// 	<img className={s.projectThumb} src={this.props.project.imageUrl} />
+    			// </Row>
+
+    			// <Row className={s.projectRow} s={4} m={12} l={12}>
+    			// 	<Col s={3} m={3} l={3}>
+	    		// 	</Col>
+	    		// 	<Col s={9} m={9} l={9}>
+	    		// 	</Col>
+    			// </Row>
     			// <Button>Check it out!</Button>
 
 export default withStyles(ProjectTile, s);
--- src/components/ProfilePage/Projects/Projects.js
+++ src/components/ProfilePage/Projects/Projects.js
@@ -4,21 +4,57 @@ import s from './Projects.scss';
 import {Row, Col} from 'react-materialize';
 import ProjectTile from './ProjectTile';
 
+var projects = [
+
+	{
+		name: "Spot Knocker",
+		description: "Peer-to-peer advertising that's rocking the ad world.",
+		imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		reference: {
+			name: "Joseph Estolas",
+			quote: "\"Shain is a champion of the free world, justice, code, and coffee.\"",
+			imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		}
+	},
+	{
+		name: "Forte",
+		description: "Sharing music performance with underprivileged youths.",
+		imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		reference: {
+			name: "Daniel Kim",
+			quote: "\"Shain is a champion of the free world, justice, code, and coffee.\"",
+			imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		}
+	},
+	{
+		name: "Capuchin",
+		description: "Split the bill with ease, like it was always meant to be.",
+		imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		reference: {
+			name: "Skyler Gonsalves",
+			quote: "\"Shain is a champion of the free world, justice, code, and coffee.\"",
+			imageUrl: "http://stanlemmens.nl/wp/wp-content/uploads/2014/07/bill-gates-wealthiest-person.jpg",
+		}
+	},
+
+];
+
+
 class Projects extends Component {
 
   render() {
     return (	
 	<div className={s.container} id='projects'>
+
+		<h1 className={s.heading}>My Projects</h1>	        	
+	
+
 		<Row>
-			<Col s={12} m={4} l={4} className="offset-l4">
-				<h1>My Projects</h1>	        	
-			</Col>
-		</Row>
-		<Row>
-		    <ProjectTile></ProjectTile>
-		    <ProjectTile></ProjectTile>
-		    <ProjectTile></ProjectTile>
-	    </Row>
+				<ProjectTile project={projects[0]}></ProjectTile>
+		    <ProjectTile project={projects[1]}></ProjectTile>
+		    <ProjectTile project={projects[2]}></ProjectTile>	
+		</Row>	
+
 	</div>
     );
   }
